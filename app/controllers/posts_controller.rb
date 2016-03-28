c4c058a23616f9a67b4930bfbc90f7d6e1f8505f
Matt <matt24ray@gmail.com>
2016-03-28 13:18:43 -0500

    crud0003

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,29 @@ class PostsController < ApplicationController
   end
 
   def show
+         @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
-  def edit
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
+
+  # def edit
+  # end
 end
