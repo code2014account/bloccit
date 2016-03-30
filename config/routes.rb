9863263426b2721f9f34544099921e434cb2a1e9
Matt <matt24ray@gmail.com>
2016-03-29 23:41:01 -0500

    crud-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'advertisements/index'
+
+  get 'advertisements/show'
+
+  get 'advertisements/new'
+
+  get 'advertisements/create'
+
   resources :posts
 
    get 'about' => 'welcome#about'
---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let (:advertisement) { Advertisement.create! }
+
+  describe "attributes" do
+    it "should respond to tile" do
+      expect(advertisement).to respond_to(:title)
+    end
+
+    it "should respond to copy" do
+      expect(advertisement).to respond_to(:copy)
+    end
+
+    it "should respond to price" do
+      expect(advertisement).to respond_to(:price)
+    end
+  end
+end
