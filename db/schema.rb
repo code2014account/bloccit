a9a8df8d4963f3fdf1e27d8acc87338162b2f657
Matt <matt24ray@gmail.com>
2016-04-09 15:07:47 -0500

    49Rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326141227) do
+ActiveRecord::Schema.define(version: 20160401143724) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20160326141227) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
